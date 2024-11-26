11af1ae9ecebd78c1e87cecc7fce6e347c242b1b
Zakharcev Fedor <zaxarvev.f@gmail.com>
2024-11-26 20:17:37 +0300

    new

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post, User, Author, Category, PostCategory, Comment
 
-# Register your models here.
+
+admin.site.register(Post)
+admin.site.register(User)
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(PostCategory)
+admin.site.register(Comment)
